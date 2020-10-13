db13cfa81d8d2215f261794af284785626712b98
Liam Craft <62984007+Crafty15@users.noreply.github.com>
2020-10-13 18:02:14 +1100

    Added a loop and counter for the bridge crossings. Not in spec, but suggested in the workshop.

--- src/Bridge.java
+++ src/Bridge.java
@@ -16,30 +16,33 @@ public class Bridge {
 		this.bridgePermit = new Semaphore(1, true);
 	}
 	//NOTE: have a "cross" method that each farmer object must access for the amount of time it takes to cross the bridge, step 1, add 200ms delay per 5 steps.
+	//NOTE: neon counter added as per lab suggestions.
 	public void cross(Farmer f) {
-		System.out.println(f.getName() + ": Waiting for bridge. Going " + f.getDirection());
-		try {
-			bridgePermit.acquire();
-			for(int i = 0; i <= LENGTH; i++) {
-				f.incStep();
-				if(i == 20) {
-					System.out.println(f.getName() + ": Across the bridge.");
-				}
-				else if(i % 5 == 0) {
-					System.out.println(f.getName() + ": Crossing bridge Step " + i);
-					//NOTE: change the sleep setting before submission
-					TimeUnit.MILLISECONDS.sleep(2000);
+		while(neon <= 100) {
+			System.out.println(f.getName() + ": Waiting for bridge. Going " + f.getDirection());
+			try {
+				bridgePermit.acquire();
+				for(int i = 0; i <= LENGTH; i++) {
+					f.incStep();
+					if(i == 20) {
+						System.out.println(f.getName() + ": Across the bridge.");
+					}
+					else if(i % 5 == 0) {
+						System.out.println(f.getName() + ": Crossing bridge Step " + i);
+						//NOTE: change the sleep setting before submission
+						TimeUnit.MILLISECONDS.sleep(200);
+					}
 				}
 			}
+			catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+			this.neon++;
+			System.out.println("NEON = " + neon);
+			//release sem, swap farmer direction
+			bridgePermit.release();
+			f.swapDirection();
 		}
-		catch(InterruptedException e) {
-			e.printStackTrace();
-		}
-		this.neon++;
-		System.out.println("NEON = " + neon);
-		//release sem, swap farmer direction
-		bridgePermit.release();
-		f.swapDirection();
 	}
 	
 	//getters
